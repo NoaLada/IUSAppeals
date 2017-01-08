43d002d112e1591ccdddc4e5e2a165556f73d5c6
NoaLada <azra.music.15@gmail.com>
2017-01-08 14:59:16 +0100

    Appeals - done and checked

--- src/routes.php
+++ src/routes.php
@@ -67,7 +67,7 @@ $app->post('/api/authenticate', function ($request, $response, $args) {
         $error = "Information missing.";
     }
 
-    return $response->withJson(array('success' => $success, 'message' => $error, 'user_type' => $user_type, 
+    return $response->withJson(array('success' => $success, 'message' => $error, 'user_type' => $user_type,
         'security_key' => $_SESSION));
 });
 
@@ -181,12 +181,12 @@ $app->delete('/api/appeals', function ($request, $response, $args) {
 });
 
 $app->get('/api/appeals/user/{id}', function ($request, $response, $args) {
-    if ($request->hasHeader('key') && 
+    if ($request->hasHeader('key') &&
         $_SESSION[$request->getHeader('key')[0]."u"] == $args['id']) {
         return getAppealsInJSON($args['id'], 0, $response);
     }
-    
-    if (check_key($request, ADMIN)) {    
+
+    if (check_key($request, ADMIN)) {
         return getAppealsInJSON($args['id'], 0, $response);
     }
 
@@ -210,11 +210,11 @@ $app->get('/api/conf', function ($request, $response, $args) {
         $success = false;
         $id = $request->hasHeader('id')[0];
         $key = $request->hasHeader('key')[0];
-        
+
         $sql = "SELECT user_id, appeal_id, sign_person_id, salt FROM signatures,user WHERE signature_id=".$id." AND user.id = signatures.user_id";
         $db = connect_db();
         $result = $db->query($sql);
-        
+
         if ($result->num_rows === 0) {
             $error = "No such appeal.";
         } else {
@@ -222,14 +222,14 @@ $app->get('/api/conf', function ($request, $response, $args) {
             while($row = mysqli_fetch_assoc($result)) {
                 $data[] = $row;
             }
-            
+
             $user_id = $data['user_id'];
             $appeal_id = $data['appeal_id'];
             $sperson_id = $data['sign_person_id'];
             $user_salt = $data['salt'];
-            
+
             $hash = generate_confirmation_hash($id, $user_id, $appeal_id, $sperson_id, $user_salt);
-            
+
             if ($hash == $key) {
                 $sql = "UPDATE signatures SET has_signed=1 WHERE signature_id=".$id.";";
                 $db = connect_db();
@@ -243,12 +243,12 @@ $app->get('/api/conf', function ($request, $response, $args) {
             } else {
                 $error = "Wrong confirmation link.";
             }
-        }    
-        
-        return $response->withJson(array('success' => $success, 'message' => $error));  
+        }
+
+        return $response->withJson(array('success' => $success, 'message' => $error));
     }
-    
-    return $response->withJson(array('success' => false, 'message' => "Missing information!"));    
+
+    return $response->withJson(array('success' => false, 'message' => "Missing information!"));
 });
 
 
